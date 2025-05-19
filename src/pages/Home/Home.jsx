80781534c329dd9cf31ecb5b7ffdf97be71f4e66
bishir tukur <bastukur1997@gmail.com>
2025-05-19 09:45:21 +0100

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,9 @@ import { FaCamera, FaHeart, FaMicrophone, FaSearch } from 'react-icons/fa'
 import image1 from "../../assets/ban1.png"
 import image2 from "../../assets/target.png"
 import image3 from "../../assets/ban2.png"
+import round1 from "../../assets/r1.png"
+import round2 from "../../assets/r2.png"
+import round3 from "../../assets/r3.png"
 
 const Home = () => {
   return (
@@ -11,7 +14,7 @@ const Home = () => {
         <div  className='lg:w-[25%] md:w-[70%] py-8 bg-white relative w-full h-[100vh]'>
           <div data-aos="fade-up" className='w-full h-[100vh] px-3'>
             <div className='flex justify-around gap-3 mb-6'>
-              <div className='flex items-center gap-2 px-2 bg-whitegray rounded-3xl shadow-md'>
+              <div className='flex items-center gap-2 px-2 py-2 bg-whitegray rounded-3xl shadow-md'>
                 <FaSearch size={20} className='hover:scale-110 hover:opacity-40'/>
                 <input type='text' name='search' placeholder=' Search Your Appliance' className='p-2 bg-whitegray outline-none text-kleinblue'/>
                 <FaMicrophone size={20} className='hover:scale-110 hover:opacity-40'/>
@@ -26,25 +29,27 @@ const Home = () => {
 
              <div className='flex justify-between mb-4'>
 
-              <div className='w-16 h-16 hover:scale-110 flex justify-center items-center rounded-full bg-whitegray border'>
+              <div className='w-20 h-20 border-red bg-opacity-50 bg-darknightblue hover:scale-95 flex justify-center items-center rounded-full border'>
                 <h1 className='text-lg font-bold'>SONY</h1>
               </div>
 
-              <div className='w-16 h-16 hover:scale-110  flex justify-center items-center rounded-full bg-whitegray border'>
-                <h1 className='text-lg font-bold'>THF</h1>
+              <div className='w-20 h-20 border-red bg-opacity-50 bg-darknightblue relative overflow-hidden hover:scale-95  flex justify-center items-center rounded-full border'>
+                <img src={round1} className='w-14'/>
               </div>
 
-              <div className='w-16 h-16 hover:scale-110  flex gap-1 justify-center items-center rounded-full bg-whitegray border'>
-               <div className='w-4 h-4 rounded-full bg-red bg-opacity-75'></div> <h1 className='text-lg font-bold'>LG</h1>
+              <div className='w-20 h-20 bg-opacity-50 border-red bg-darknightblue relative overflow-hidden hover:scale-95  flex justify-center items-center rounded-full border'>
+                <img src={round2} className='w-14'/>
               </div>
 
-              <div className='w-16 h-16 hover:scale-110  flex justify-center items-center rounded-full bg-whitegray border'>
-                <h1 className='text-lg font-bold'>Supt</h1>
+              <div className='w-20 h-20 bg-opacity-50 bg-darknightblue border-red relative overflow-hidden hover:scale-95  flex justify-center items-center rounded-full border'>
+                <img src={round3} className='w-14'/>
               </div>
+
+             
              </div>
               
               <div>
-                <div data-aos="zoom-in" className='w-[100%] h-[20vh] overflow-hidden relative bg-red bg-opacity-55 rounded-2xl'>
+                <div data-aos="zoom-in" className='w-[100%] h-[20vh] mb-4 overflow-hidden relative bg-red bg-opacity-55 rounded-2xl'>
                   {/* Animated */}
                   <div className='flex justify-between absolute'>
                     <div className='w-4 h-4 rounded-full bg-darknightblue animate-ping'></div>
@@ -65,6 +70,8 @@ const Home = () => {
                       <img src={image3} className='scale-125 relative top-10 -left-4'/>
                   </div>
                 </div>
+
+                <p className=' text-lg font-semibold mb-2'>Recommended For You</p>
               </div>
           </div>
           <div><Navigation/></div>
